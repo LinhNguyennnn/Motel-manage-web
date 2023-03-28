f7e29ae46beac6398ed397a28dbfb726f72f1d71
tungnt <tung120320@gmail.com>
2023-03-28 16:27:22 +0700

    change env

--- src/pages/api/instance.tsx
+++ src/pages/api/instance.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8800/api',
+  baseURL: process.env.NEXT_PUBLIC_BASE_API_LOCAL,
 });
 export default instance;
